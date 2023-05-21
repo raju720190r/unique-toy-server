75f654c6165a9a9d6f25d701e27a01f4be4a76a4
raju720190r <raju720190r@gmail.com>
2023-05-21 17:46:12 +0600

    Delete A single Items

--- index.js
+++ index.js
@@ -84,6 +84,15 @@ async function run() {
     })
 
 
+    // Delete 
+    app.delete('/toys/:id',async(req,res)=>{
+      const id =req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result =await toysCollection.deleteOne(query);
+      res.send(result);
+    })
+
+
    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
